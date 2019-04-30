768a03857c7b908a352d0a0e16b0146492e17c5a
Fehér Ágnes <feheragnes1998@gmail.com>
2019-04-30 19:00:37 +0200

    defense stats

---
+++ src/app/interfaces/defense.ts
@@ -0,0 +1,16 @@
+export interface Sack {
+  name: string;
+  team: string;
+  sacksNumber: number;
+}
+export interface Tackle {
+  name: string;
+  team: string;
+  tacklesNumber: number;
+}
+
+export interface Interception {
+  name: string;
+  team: string;
+  interceptionsNumber: number;
+}
--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -6,6 +6,7 @@ import { Dline } from '../interfaces/dline';
 import { Secondary } from '../interfaces/secondary';
 import { FirstDown } from '../interfaces/firstdown';
 import { TdYpg } from '../interfaces/tdypg';
+import { Tackle, Sack, Interception } from '../interfaces/defense';
 
 @Injectable({
   providedIn: 'root'
@@ -33,5 +34,31 @@ export class ChartService {
   }
   getTdYpg(season: number): Observable<TdYpg[]> {
     return this.http.get<TdYpg[]>(`${this.tdypgUrl}/${season}`);
+  }
+
+  getSacks(season: number): Observable<Sack[]> {
+    return this.http.get<Sack[]>(
+      `https://apiv2dev.nkelemen.hu/sacks/${season}`
+    );
+  }
+
+  getTackles(season: number): Observable<Tackle[]> {
+    return this.http.get<Tackle[]>(
+      `https://apiv2dev.nkelemen.hu/tackles/${season}`
+    );
+  }
+
+  getInterceptions(season: number): Observable<Interception[]> {
+    return this.http.get<Interception[]>(
+      `https://apiv2dev.nkelemen.hu/interceptions/${season}`
+    );
+  }
+
+  getRb(season: number): Observable<[]> {
+    return this.http.get<[]>(`https://apiv2dev.nkelemen.hu/rb/${season}`);
+  }
+
+  getQb(season: number): Observable<[]> {
+    return this.http.get<[]>(`https://apiv2dev.nkelemen.hu/qb/${season}`);
   }
 }
